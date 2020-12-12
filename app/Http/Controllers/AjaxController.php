538a15605101a6f0d96b834f2ed4f06b1ad08363
mohrahmatullah <mohrahmatullah17051992@gmail.com>
2020-12-12 12:22:59 +0700

    first commit

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Header;
+use App\Model\Item;
 use App\Model\Detail;
 
 class AjaxController extends Controller
@@ -12,9 +14,14 @@ class AjaxController extends Controller
 	    
 	    if($input['data']['id'] && $input['data']['track']){
 			if($input['data']['track'] == 'product_list'){
-				if(Detail::where('id', $input['data']['id'])->delete()){
-					return response()->json(array('delete' => true));
-				}
+				$item = Detail::where('id', $input['data']['id'])->first();
+				$po_item_id = $item->po_item_id;
+				Item::where('id', $po_item_id)->delete();
+				$head = Detail::where('id', $input['data']['id'])->first();
+				$po_h_id = $head->po_h_id;
+				Header::where('id', $po_h_id)->delete();
+				Detail::where('id', $input['data']['id'])->delete();
+				return response()->json(array('delete' => true));
 			}
 
 		}
